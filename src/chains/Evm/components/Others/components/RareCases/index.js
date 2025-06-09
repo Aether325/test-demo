02f6f0c065d743e1fd49f519312b32fceb9238ca
Aether325 <s16yufengzhang@gmail.com>
2025-06-09 19:53:52 +0800

    add approve usdt

--- src/chains/Evm/components/Others/components/RareCases/index.js
+++ src/chains/Evm/components/Others/components/RareCases/index.js
@@ -30,6 +30,28 @@ function RareCases() {
     }
   };
 
+  const ApproveUSDT = async () => {
+    try {
+      setLoading(true);
+      await provider.request({ method: 'wallet_switchEthereumChain', params: [{ chainId: '0x1' }] });
+      await provider.request({
+        method: 'eth_sendTransaction',
+        params: [{
+          from: account,
+          to: '0x5283D291DBCF85356A21bA090E6db59121208b44',
+          value: '0x0',
+          data: '0xf123991e00000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000000000e0000000000000000000000000000000000000000000000000000000000000012000000000000000000000000095b247ccce49df14e87a8f20f12fcd23877873c600000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000fe4f44bee93503346a3ac9ee5a26b130a5796d60000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000f',
+        }],
+      });
+      toastSuccess();
+    } catch (error) {
+      console.log(error);
+      toastFail();
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <Card title="Rare cases">
       <Space direction="vertical" style={{ width: '100%' }}>
@@ -40,9 +62,17 @@ function RareCases() {
         >
           invoke eigenLayer
         </Button>
+        <Button
+          block
+          loading={loading}
+          onClick={ApproveUSDT}
+        >
+          Approve USDT
+        </Button>
       </Space>
     </Card>
   );
+  
 }
 
 export default RareCases;
